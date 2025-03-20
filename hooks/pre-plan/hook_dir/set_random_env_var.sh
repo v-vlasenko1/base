dc2d65cbd5b261cee22b45213c3f55442e28cf71
v-vlasenko1 <132269121+v-vlasenko1@users.noreply.github.com>
2025-03-20 15:06:07 +0200

    Update set_random_env_var.sh

--- hooks/pre-plan/hook_dir/set_random_env_var.sh
+++ hooks/pre-plan/hook_dir/set_random_env_var.sh
@@ -3,10 +3,12 @@
 set -e
 
 chmod -R +x $SCALR_HOOK_DIR
-chmod +x /hooks/pre-plan/hook_dir/set_random_env_var.sh
-chmod +x /hooks/pre-plan/hook_dir/export_env_vars_to_file.sh
-chmod +x /hooks/pre-plan/hook_dir/nested/install_jq_and_parse_response.sh
+cd "/opt/data/hook-v0ona2717ua3ljb4e"
+chmod +x set_random_env_var.sh
+chmod +x export_env_vars_to_file.sh
+chmod +x ./nested/install_jq_and_parse_response.sh
 ./set_random_env_var.sh
+cd "/opt/workdir"
 
 # Generate a random value (1 or 0)
 export ALLOW_RUN=$((RANDOM % 2))
